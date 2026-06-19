263484a9283a54500f4aace0543161fd360c1a63
Lien04 <trinhlien10101@gmail.com>
2026-06-19 20:08:15 +0700

    push code day 19

---
+++ src/main/java/feature/ToolsAPI/GetAllOrders.feature
@@ -0,0 +1,4 @@
+Feature: ToolAPITesting
+Scenario: Check response when get all orders
+When request to get all orders
+Then return all orders
---
+++ src/main/java/stepsdefinition/ToolsAPI/GetAllOrdersSteps.java
@@ -0,0 +1,14 @@
+package stepsdefinition.ToolsAPI;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GetAllOrdersSteps {
+	@When("Reuqest to get all orders")
+	public void request_to_get_all_orders() {	
+	}
+
+	@Then("Return all orders")
+	public void return_all_orders() {
+	}
+}
