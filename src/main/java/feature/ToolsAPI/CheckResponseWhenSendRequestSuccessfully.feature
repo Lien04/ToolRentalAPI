1c20c3338ccfa9fe25cffd8168ef8c36187d5737
Lien04 <trinhlien10101@gmail.com>
2026-07-03 20:44:08 +0700

    push code of day 21

--- src/main/java/feature/ToolsAPI/CheckResponseWhenSendRequestSuccessfully.feature
+++ src/main/java/feature/ToolsAPI/CheckResponseWhenSendRequestSuccessfully.feature
@@ -2,12 +2,12 @@ Feature: ToolsAPITesting
 
   Scenario Outline: Check response when send request successfully
     Given I have header
-      | key               | value              |
-      | "Accept-Encoding" | "gzip,deflate,br"  |
-      | Accept            | "application/json" |
+      | key             | value            |
+      | Accept-Encoding | gzip,deflate,br  |
+      | Accept          | application/json |
     Given I have url and method
-      | url                                                                                       | method |
-      | "https://simple-tool-rental-api.glitch.me?category=@category&result&available=@available" | GET    |
+      | url                                                                                             | method |
+      | https://simple-tool-rental-api.glitch.me?/tools?category=@category&@result&available=@available | GET    |
     Given I have "<category>" and "<result>" of tools and "<available>" status
     When send request with valid URL and method and params
     Then Api responds status code "<StatusCode>" and list of tools
--- src/main/java/stepsdefinition/ToolsAPI/CheckResponseWhenSendRequestSuccessfullySteps.java
+++ src/main/java/stepsdefinition/ToolsAPI/CheckResponseWhenSendRequestSuccessfullySteps.java
@@ -1,10 +1,17 @@
 package stepsdefinition.ToolsAPI;
 
+import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import common.RequestUtils;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
@@ -12,9 +19,13 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CheckResponseWhenSendRequestSuccessfullySteps {
+	private static final String jsonString = null;
 	String url;
 	String method;
 	String newUrl;
+	String category;
+	int result;
+	String available;
 	HttpResponse<String> response;
 	Map<String, String> headers = new HashMap<String, String>();
 	@Given("I have header")
@@ -36,9 +47,14 @@ public class CheckResponseWhenSendRequestSuccessfullySteps {
 	
 	@Given("I have {string} and {string} of tools and {string} status")
 	public void i_have_and_of_tools_and_status(String givenCategory, String givenResult, String givenAvailable) {
+		category = givenCategory;
+		result = Integer.parseInt(givenResult);
+		available = givenAvailable;
 //		url = "https://simple-tool-rental-api.glitch.me?category=" + givenCategory + "&results=" + givenResult
 //				+ "&available=" + givenAvailable;
+//		System.out.println(url);
 		newUrl = url.replace("@category", givenCategory).replace("@results", givenResult).replace("@available", givenAvailable);
+//		System.out.println(newUrl);
 	}
 
 	@When("send request with valid URL and method and params")
@@ -61,6 +77,34 @@ public class CheckResponseWhenSendRequestSuccessfullySteps {
 	@Then("Api responds status code {string} and list of tools")
 	public void api_responds_status_code_and_list_of_tools(String expectedStatusCode) {
 		assertEquals(Integer.parseInt(expectedStatusCode), response.statusCode());
-	}
-
+		int actualToolsNumber = 0;
+		int actualValidIDNumber = 0;
+		int actualValidCategoryNumber = 0;
+		int actualStockNumber = 0;
+		String responseJson = response.body();
+		JSONParser parser = new JSONParser();
+		try {
+			Object responseObj = parser.parse(responseJson);
+			if (responseObj instanceof JSONArray) {
+				JSONArray responseArray = (JSONArray) responseObj;
+				actualToolsNumber = responseArray.size();
+				for (Object toolObj : responseArray) {
+					JSONObject toolJsonObject = (JSONObject) toolObj;
+					String cat = toolJsonObject.get("category").toString();
+					if(cat.equals(category)) {
+						actualValidCategoryNumber++;
+					}
+					String instock = toolJsonObject.get("inStock").toString();
+					if(instock.equals(available)) {
+						actualStockNumber++;
+					}
+				}
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		assertTrue(actualToolsNumber <= result);
+		assertEquals(actualToolsNumber, actualValidIDNumber);
+		assertEquals(actualValidCategoryNumber, actualToolsNumber);
+		}
 }
