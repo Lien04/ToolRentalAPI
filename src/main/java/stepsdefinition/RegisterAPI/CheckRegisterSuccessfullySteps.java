dbaf672a8e6298544d21a8a9f6a2d13b60d64204
Lien04 <trinhlien10101@gmail.com>
2026-07-04 16:44:45 +0700

    push code of day 22

--- src/main/java/stepsdefinition/RegisterAPI/CheckRegisterSuccessfullySteps.java
+++ src/main/java/stepsdefinition/RegisterAPI/CheckRegisterSuccessfullySteps.java
@@ -3,12 +3,15 @@ package stepsdefinition.RegisterAPI;
 import static org.testng.AssertJUnit.assertEquals;
 
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 import common.JsonUtils;
+import common.RequestUtils;
 import common.ScenarioContext;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class CheckRegisterSuccessfullySteps {
 	String requestBody;
@@ -27,15 +30,15 @@ public class CheckRegisterSuccessfullySteps {
 		requestBody = jsonUtils.readJsonFile(requestBodyName);
 	}
 	
-//	@When("send request")
-//	public void send_request_with_valid_url_and_method_and_params() {
-//		RequestUtils req = new RequestUtils();
-//		String url = (String) scenarioContext.getContextByKey("url");
-//		String method = (String) scenarioContext.getContextByKey("method");
-//		Map<String, String> headers = (Map<String, String>) scenarioContext.getContextByKey("headers");
-//		response = req.sendRequest(url, method, headers, requestBody);
-//		
-//	}
+	@When("send request")
+	public void send_request_with_valid_url_and_method_and_params() {
+		RequestUtils request = new RequestUtils();
+		String url = (String) scenarioContext.getContextByKey("url");
+		String method = (String) scenarioContext.getContextByKey("method");
+		Map<String, String> headers = (Map<String, String>) scenarioContext.getContextByKey("headers");
+		response = request.sendRequest(url, method, headers, requestBody);
+		
+	}
 	
 	@Then("Api responds status code {string}")
 	public void api_responds_status_code(String expectedStatusCode) {
