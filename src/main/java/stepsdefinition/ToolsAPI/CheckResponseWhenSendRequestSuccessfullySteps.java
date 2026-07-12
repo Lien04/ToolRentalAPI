8bc25e0f3aa4379781fb4890a12061e0c0cee4ef
Lien04 <trinhlien10101@gmail.com>
2026-07-12 10:19:04 +0700

    push code day 22

--- src/main/java/stepsdefinition/ToolsAPI/CheckResponseWhenSendRequestSuccessfullySteps.java
+++ src/main/java/stepsdefinition/ToolsAPI/CheckResponseWhenSendRequestSuccessfullySteps.java
@@ -3,16 +3,13 @@ package stepsdefinition.ToolsAPI;
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import common.Context;
+import common.JsonUtils;
 import common.RequestUtils;
 import common.ScenarioContext;
 import io.cucumber.datatable.DataTable;
@@ -30,10 +27,10 @@ public class CheckResponseWhenSendRequestSuccessfullySteps {
 	HttpResponse<String> response;
 	Map<String, String> headers = new HashMap<String, String>();
 	private final ScenarioContext scenarioContext;
-	
+
 	public CheckResponseWhenSendRequestSuccessfullySteps(ScenarioContext scenarioContext) {
 		this.scenarioContext = scenarioContext;
-		
+
 	}
 
 	@Given("I have header")
@@ -42,7 +39,7 @@ public class CheckResponseWhenSendRequestSuccessfullySteps {
 		for (Map<String, String> header : originalHeaders) {
 			String key = header.get("key");
 			String value = header.get("value");
-			headers.put(key,value);
+			headers.put(key, value);
 		}
 		scenarioContext.setContext(Context.HEADERS, headers);
 	}
@@ -51,64 +48,55 @@ public class CheckResponseWhenSendRequestSuccessfullySteps {
 	public void i_have_url_and_method(DataTable urlAndMethodTable) {
 		List<Map<String, String>> originalUrlMethods = urlAndMethodTable.asMaps(String.class, String.class);
 		url = originalUrlMethods.get(0).get("url");
-		scenarioContext.setContext(Context.URL ,url);
+		scenarioContext.setContext(Context.URL, url);
 		method = originalUrlMethods.get(0).get("method");
 		scenarioContext.setContext(Context.METHOD, method);
 	}
-	
+
 	@Given("I have {string} and {string} of tools and {string} status")
 	public void i_have_and_of_tools_and_status(String givenCategory, String givenResult, String givenAvailable) {
 		category = givenCategory;
 		result = Integer.parseInt(givenResult);
 		available = givenAvailable;
-		newUrl = url.replace("@category", givenCategory).replace("@results", givenResult).replace("@available", givenAvailable);
+		newUrl = url.replace("@category", givenCategory).replace("@results", givenResult).replace("@available",
+				givenAvailable);
 	}
 
 	@When("send request")
 	public void send_request_with_valid_url_and_method_and_params() {
 		RequestUtils req = new RequestUtils();
 		response = req.sendRequest(newUrl, method, headers, "");
-		
+
 	}
 
 	@Then("Api responds status code {string} and list of tools")
 	public void api_responds_status_code_and_list_of_tools(String expectedStatusCode) {
 		assertEquals(Integer.parseInt(expectedStatusCode), response.statusCode());
-		int actualToolNumber = 0;
-		int actualValidIDNumber = 0;
-		int actualValidCategoryNumber = 0;
-		int actualStockNumber = 0;
 		String responseJson = response.body();
-		JSONParser parser = new JSONParser();
-		try {
-			Object responseObj = parser.parse(responseJson);
-			if (responseObj instanceof JSONArray) {
-				JSONArray resArray = (JSONArray) responseObj;
-				actualToolNumber = resArray.size();
-				for (Object toolObj : resArray) {
-					JSONObject toolJsonObject = (JSONObject) toolObj;
-					String idTool = toolJsonObject.get("id").toString();
-					if (!(idTool.isBlank() || idTool.isEmpty())) {
-						actualValidIDNumber++;
-					}
-					String cat = toolJsonObject.get("category").toString();
-					if (cat.equals(category)) {
-						actualValidCategoryNumber++;
-					}
-					String instock = toolJsonObject.get("inStock").toString();
-					if (instock.equals(available)) {
-						actualStockNumber++;
-					}
-				}
+		JsonUtils jsonUtils = new JsonUtils();
+		ArrayList<String> ids = jsonUtils.getDataByKey(responseJson, "id");
+		ArrayList<String> cats = jsonUtils.getDataByKey(responseJson, "category");
+		ArrayList<String> instocks = jsonUtils.getDataByKey(responseJson, "inStock");
+
+		assertTrue(ids.size() <= result);
+		assertEquals(cats.size(), ids.size());
+		assertEquals(instocks.size(), ids.size());
+		boolean isCatChecked = true;
+		for (String cat : cats) {
+			if (!cat.equals(category)) {
+				isCatChecked = false;
+				break;
 			}
-		
-		} catch (ParseException e) {
-			e.printStackTrace();
 		}
-		assertTrue(actualToolNumber <= result);
-		assertEquals(actualValidIDNumber, actualToolNumber);
-		assertEquals(actualValidCategoryNumber, actualToolNumber);
-		assertEquals(actualStockNumber, actualToolNumber);
+		assertTrue(isCatChecked);
+		boolean isStockChecked = true;
+		for (String stock : instocks) {
+			if (!stock.equals(available)) {
+				isStockChecked = false;
+				break;
+			}
+		}
+		assertTrue(isStockChecked);
 	}
 
 }
